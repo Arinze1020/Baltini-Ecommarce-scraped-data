3a0e6ed8e27abb95be76dce2899ee58b68ba0f4a
Arinze <33836764+Arinze1020@users.noreply.github.com>
2024-05-15 22:34:52 +0100

    Update baltini.py
    update

--- baltini.py
+++ baltini.py
@@ -1,24 +1,43 @@
-import re
-from playwright.sync_api import Playwright, sync_playwright, expect
+import os
+from bs4 import BeautifulSoup
+products = []
+for filename in os.listdir("baltini"):
+        if filename.endswith(".html"):
+            file_path = os.path.join("baltini", filename)
+            with open(file_path, 'rb') as f:
+                print(f)
+                soup = BeautifulSoup(f.read(), 'lxml')
+                data = soup.find('ul', class_='snize-search-results-content clearfix')
+                
 
+                items = data.find_all('li') # Use a common container
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://www.google.com/")
-    page.get_by_label("Search", exact=True).fill("Nordstrom Rack")
-    page.get_by_role("link", name="Nordstrom Rack: Shop Clothes").click()
-    page.get_by_role("link", name="View All Top 100 Deals").click()
-    page.goto("https://www.nordstromrack.com/shop/top-100-deals?origin=recs-hp-top-100-deals-rack")
+                for item in items:
+                    name = item.find('span', class_='snize-title').text.strip()
+                    link = "https://www.baltini.com"+item.find('a', class_='snize-view-link')['href']
+                    img = item.find('img', class_='snize-item-image')['src']
 
-    with open('saving_nord.html', 'w',encoding='utf-8') as f:
-        f.write(page.content())
+                    first_price = item.find('span', class_='snize-price snize-price-with-discount')
+                    price = first_price.text.strip() if first_price else 'NA'
 
-    # ---------------------
-    context.close()
-    browser.close()
+                    dis_count = item.find('span', class_="snize-discounted-price")
+                    discount = dis_count.text.strip() if dis_count else 'NA'
 
+                    brand_id = item.find('span', class_='snize-attribute').text.strip()
+                    idd = item.get('id')
 
-with sync_playwright() as playwright:
-    run(playwright)
+                    product = {
+                        'brand': brand_id,
+                        'product_code':idd,
+                        'full_price': discount,
+                        'price': price,
+                        'title': name,
+                        'imageurl': img,
+                        'itemurl': link,
+                        
+                    }
+                    products.append(product)
+                print(len(products))
+import pandas as pd
+df = pd.DataFrame(products)
+df.to_csv('data_blint.csv')
